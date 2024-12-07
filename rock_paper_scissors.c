1221188e8c11ff7f37b89e771a060df10b244e30
Yılmaz Sönmez <ysonmez824@gmail.com>
2024-12-07 23:07:13 +0300

    Update rock_paper_scissors.c

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -11,9 +11,11 @@ int main()
     
     printf("\t\tROCK-PAPER-SCISSORS\n\n");
     
+    //Committing loop in case player want's to continue
+    
     while(1){
     
-    printf("\tPlease select one of the following;\n");
+    printf("\tPlease select one of the following (1-2-3);\n");
     
     printf("\t\t1.Rock\n\t\t2.Paper\n\t\t3.Scissors\n\n\t\t  ");
     
@@ -21,33 +23,35 @@ int main()
     
     select_b = rand() % 3 + 1;
     
+    //Cases that can be happen
+    
     if(select_b == select_p)
        
         printf("DRAW!!");
     
     else if(select_p == 1 && select_b == 2)
         
-        printf("\aRock wrapped by paper! You have Lost!");
+        printf("\n\aRock wrapped by paper! You have Lost!");
     
     else if (select_p == 1 && select_b == 3)
     
-        printf("Rock breaks Scissors! You have Won!");
+        printf("\n\aRock breaks Scissors! You have Won!");
     
     else if (select_p == 2 && select_b == 1)
         
-        printf("Paper wraps rock! You have Won!");
+        printf("\n\aPaper wraps rock! You have Won!");
         
     else if(select_p == 2 && select_b == 3)
     
-        printf("\aPaper turned into a snowflake by Scissors! You have Lost!");
+        printf("\n\aPaper turned into a snowflake by Scissors! You have Lost!");
         
     else if(select_p == 3 && select_b == 1)
     
-        printf("\aScissors smashed brutaly by the great force of Rock! You have Lost!");
+        printf("\n\aScissors smashed brutaly by the great force of Rock! You have Lost!");
         
     else if (select_p == 3 && select_b == 2)
     
-        printf("Scissors cuts through Paper! You have Won!");
+        printf("\n\aScissors cuts through Paper! You have Won!");
         
     else {
         
@@ -55,7 +59,9 @@ int main()
         exit(1);
     }
     
-    printf("\n\tDo you want to continue? (Y/N)\n\t");
+    //Asking player if he/she would like to replay
+    
+    printf("\n\tDo you want to restart? (Y/N)\n\t");
     
     scanf(" %c", &slct);
     
